6fda27572987f97f450989275314346abdf55e2f
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-07-14 10:45:59 +0700

    swr

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,17 +1,24 @@
 import { GetStaticProps, GetStaticPropsContext } from "next";
 import Link from "next/link";
 import React from "react";
+import useSWR from "swr";
 
 type ProductsProps = {
   products: any[];
 };
 
+const url = "http://localhost:3001/products";
+
+const fetcher = async () => await (await fetch(url)).json();
+
 // client
-const ProductPage = ({ products }: ProductsProps) => {
-  if (!products) return null;
+const ProductPage = () => {
+  const { data, error } = useSWR(url, fetcher, { dedupingInterval: 5000 });
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   return (
     <div>
-      {products.map((item) => (
+      {data.map((item: any) => (
         <div key={item.id}>
           <Link href={`/products/${item.id}`}>{item.name}</Link>
         </div>
